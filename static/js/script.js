0b447dbf5aa7c0c3cadee5fc881e269049e85fde
Kirill <azee12062006@gmail.com>
2023-11-22 17:06:15 +0300

    Style changes

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,31 @@
-$(document).ready(function(){
-  //Инициализация всплывающей панели
-  $('[data-toggle="popover"]').popover({
-    placement : 'top'
-  });
-});
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+function selectCardF(card) {
+      var cards = document.getElementsByClassName("f");
+      for (var i = 0; i < cards.length; i++) {
+        cards[i].classList.remove("selected");
+      }
+      card.classList.add("selected");
+}
+function selectCardS(card) {
+      var cards = document.getElementsByClassName("s");
+      for (var i = 0; i < cards.length; i++) {
+        cards[i].classList.remove("selected");
+      }
+      card.classList.add("selected");
+}
+function selectCardSL(card) {
+      var cards = document.getElementsByClassName("sl");
+      for (var i = 0; i < cards.length; i++) {
+        cards[i].classList.remove("selected");
+      }
+      card.classList.add("selected");
+}
+function selectCardd(card) {
+      var cards = document.getElementsByClassName("d");
+      for (var i = 0; i < cards.length; i++) {
+        cards[i].classList.remove("selected");
+      }
+      card.classList.add("selected");
+}
